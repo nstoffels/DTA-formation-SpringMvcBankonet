0adc64145f2dd7d8b9f74a61a7a61116242e7ebf
n.stoffels@groupe-esigelec.fr <n.stoffels@groupe-esigelec.fr>
2015-07-29 20:25:17 +0200

    travail maison

--- src/main/java/com/bankonet/dao/CompteDaoImpl.java
+++ src/main/java/com/bankonet/dao/CompteDaoImpl.java
@@ -8,11 +8,13 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.bankonet.model.Client;
 import com.bankonet.model.Compte;
 
 /**
@@ -21,10 +23,10 @@ import com.bankonet.model.Compte;
  */
 @Repository("comptedao")
 public class CompteDaoImpl implements ICompteDao {
-	
+
 	@PersistenceContext
 	private EntityManager em;
-	
+
 	/*
 	 * 
 	 * Constructeur
@@ -37,7 +39,7 @@ public class CompteDaoImpl implements ICompteDao {
 		// TODO Auto-generated constructor stub
 	}
 
-	
+
 	/*
 	 * 
 	 * 
@@ -56,7 +58,7 @@ public class CompteDaoImpl implements ICompteDao {
 	/* (non-Javadoc)
 	 * @see com.bankonet.dao.ICompteDao#deleteCompte(int)
 	 */
-	@Override
+	@Transactional(propagation=Propagation.MANDATORY)
 	public void deleteCompte(int idCompte) {
 		// TODO Auto-generated method stub
 		Compte c=em.find(Compte.class, idCompte);
@@ -67,33 +69,50 @@ public class CompteDaoImpl implements ICompteDao {
 	/* (non-Javadoc)
 	 * @see com.bankonet.dao.ICompteDao#crediterCompte(float)
 	 */
-	@Override
+	@Transactional(propagation=Propagation.MANDATORY)
 	public void crediterCompte(float solde) {
 		// TODO Auto-generated method stub
-
+		em.merge(solde);
 	}
 
 	/* (non-Javadoc)
 	 * @see com.bankonet.dao.ICompteDao#debiterCompte(float)
 	 */
-	@Override
+	@Transactional(propagation=Propagation.MANDATORY)
 	public void debiterCompte(float solde) {
 		// TODO Auto-generated method stub
-		
+		em.merge(solde);
 	}
 
 	/* (non-Javadoc)
 	 * @see com.bankonet.dao.ICompteDao#listeCompte()
 	 */
-	@Override
+	@Transactional(propagation=Propagation.MANDATORY)
 	public List<Compte> listeCompte() {
 		// TODO Auto-generated method stub
 		Query req=em.createQuery("select c from Compte c order by c.id");
 		System.out.println("liste compte : " + req.getResultList());
 		return req.getResultList();
 	}
+	
+	/* (non-Javadoc)
+	 * @see com.bankonet.dao.ICompteDao#listeCompteclient()
+	 */
+	@Transactional(propagation=Propagation.MANDATORY)
+	public List<Compte> listCompteclient(Client c) {  
+
+		TypedQuery<Compte> selectQuery = em.createQuery("select compte FROM Compte as compte where compte.clientcompte = :x",Compte.class);  
+		selectQuery.setParameter("x", c.getId());  
+
+		return (List<Compte>)selectQuery.getResultList();  
+	}  
 
 
+	/*
+	 * 
+	 * génération des getters et setters
+	 * 
+	 */
 	/**
 	 * @return the em
 	 */
@@ -108,8 +127,8 @@ public class CompteDaoImpl implements ICompteDao {
 	public void setEm(EntityManager em) {
 		this.em = em;
 	}
-	
-	
-	
+
+
+
 
 }
